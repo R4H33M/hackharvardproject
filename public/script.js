a38d14725dcbed18f90d7b7daae51e939d352cd0
Raheem Idowu <--global>
2022-10-15 16:07:03 -0400

    sprint stage 1

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
-console.log("helo")
-let socket = io();
-socket.on('connect', () => {
+function namesubmitted() {
+    console.log("helo")
+    let socket = io();
+    socket.on('connect', () => {
         console.log("client connected via sockets");
         let data = {
-            "name": "raheem",
-            "room": 19
+            "name": document.getElementById("uname").value,
+            "room": document.getElementById("psw").value
         }
         socket.emit('userData', data);
     })
-    // recieve prev msgs
+    document.getElementById("firstform").style.display = "none";
+}
+
+
+    // recieve prev msgs 
