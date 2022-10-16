77e22f0aadbad016471c92ca80d3e885d412536b
wli-linda <wli62442@gmail.com>
2022-10-15 20:56:57 -0400

    update namelist display

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-function namesubmitted() {
-    console.log("helo")
+function enterRoom() {
+    // console.log("helo")
     let socket = io();
     socket.on('connect', () => {
         console.log("client connected via sockets");
@@ -10,9 +10,20 @@ function namesubmitted() {
         socket.emit('userData', data);
     })
 
-    document.getElementById("firstform").style.display = "none";
+    document.getElementById("loginPage").style.display = "none";
 
     socket.on('namelist', (data) => {
         console.log("namelist", data);
+        displayList(namelist);
     })
+}
+
+function displayList(namelist) {
+    console.log("bla2: ",namelist)
+    let html = ""
+    for (username in namelist) {
+        html += "<li>" + username + "</li>";
+    }
+    document.getElementById("namelist").innerHTML += html;
+    document.getElementById("namelist").style.display = "flex";
 }
